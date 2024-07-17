dcd4fe279d41f7a30792621a37118587b018a811
deepika <vkdeepika5gmail.com>
2024-07-17 14:47:33 +0530

    new project

---
+++ public/main.js
@@ -0,0 +1,98 @@
+const socket = io()
+
+const clientsTotal = document.getElementById('client-total')
+const messageContainer=document.getElementById('message-container')
+const nameInput=document.getElementById('name-input')
+const messageForm=document.getElementById('message-form')
+const messageInput=document.getElementById('message-input')
+const messageTone=new Audio('/message-tone.mp3')
+const currentDateTime = new Date();
+
+// Format the date and time
+const formattedDateTime = `${currentDateTime.toLocaleDateString()} ${currentDateTime.toLocaleTimeString()}`;
+messageForm.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  sendMessage()
+})
+socket.on('clients-total', (data) => {
+    clientsTotal.innerText = `Total Clients: ${data}`
+  })
+
+  function sendMessage() {
+    if (messageInput.value === '') return
+    // console.log(messageInput.value)
+    const data = {
+      name: nameInput.value,
+      message: messageInput.value,
+      dateTime: formattedDateTime,
+    }
+    console.log(data); 
+    socket.emit('message', data)
+    addMessageToUI(true, data)
+    messageInput.value = ''
+  }
+  
+  socket.on('chat-message', (data) => {
+    // console.log(data)
+    messageTone.play()
+    addMessageToUI(false, data)
+  })
+  
+  function addMessageToUI(isOwnMessage, data) {
+    // Clear feedback messages if any
+    clearFeedback();
+
+    // If data.dateTime is not provided, use the current date and time
+    const dateTime = data.dateTime || new Date();
+
+    // Create the message element
+    const element = `
+        <li class="${isOwnMessage ? 'message-right' : 'message-left'}">
+            <p class="message">
+              ${data.message}
+              <span>${data.name} ● ${data.dateTime}</span>
+            </p>
+        </li>
+    `;
+
+    // Add the message to the message container
+    messageContainer.innerHTML += element;
+
+    // Scroll to the bottom of the message container
+    scrollToBottom();
+}
+
+  function scrollToBottom(){
+    messageContainer.scrollTo(0,messageContainer.scrollHeight)
+  }
+  messageInput.addEventListener('focus',(e)=>{
+    socket.emit('feedback',{
+      feedback:`✍${nameInput.value} is typing a message `,
+    })
+  })
+  messageInput.addEventListener('keypress', (e)=> {
+    socket.emit('feedback',{
+      feedback:`✍${nameInput.value} is typing a message `,
+    })
+   })
+  messageInput.addEventListener('blur', (e)=>{ 
+    socket.emit('feedback',{
+      feedback:'',
+    })
+  })
+
+  socket.on('feedback',(data)=> {
+    clearFeedback()
+    const element=`<li class="message-feedback">
+                <p class="feedback" id="feedback">
+                    ${data.feedback}
+                </p>
+            </li>`
+            messageContainer.innerHTML+=element
+  })
+
+  function clearFeedback(){
+    document.querySelectorAll('li.message-feedback').forEach(element=> {
+      element.parentNode.removeChild(element)
+    })
+  }
